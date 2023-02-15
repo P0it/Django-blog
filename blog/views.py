c4b360fff7a50034cd75e39025206390ff264107
snetHyunwoo <fanning9386@gmail.com>
2023-02-15 14:17:29 +0900

    post orderby

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .models import Post
 # Create your views here.
 
 def index(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('-pk')
 
     return render(
         request,
